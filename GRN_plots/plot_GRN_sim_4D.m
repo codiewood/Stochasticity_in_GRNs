4d938b0f9cf19c50ea744e7380f6174d1d05c1d2
codiewood <52531428+codiewood@users.noreply.github.com>
2020-07-08 11:25:54 +0100

    wrote script to plot ODE/SSA simulations

--- GRN_plots/plot_GRN_sim_4D.m
+++ GRN_plots/plot_GRN_sim_4D.m
@@ -1,8 +1,15 @@
-GRN_sim_4D.m
+%simulate SSA and ODE model
+inputs = {1,1,1,1};
+[ssa_t, ssa_simdata, ssa_names] = SSA_4D_GRN(inputs);
+[ode_t, ode_simdata, ode_names] = ODE_4D_GRN(inputs);
 
+%plots figure
 figure;
-plot(t, simdata(:,[2,7,12,17]), 'Linewidth',1.5)
-xlabel('Time')
-ylabel('Molecule number')
-title('Number vs Time')
-legend('Protein a','Protein b','Protein c','Protein d')
+    plot(ssa_t, ssa_simdata(:,[2,7,12,17]), '-' , 'LineWidth', 1.5)
+    hold on
+    plot(ode_t, ode_simdata(:,[2,7,12,17]), ':', 'LineWidth', 3)
+    xlabel('Time (secs)')
+    ylabel('Molecule number')
+    title('Molecule numbers vs Time')
+    legend('SSA Protein a','SSA Protein b','SSA Protein c','SSA Protein d', ...
+        'ODE Protein a', 'ODE Protein b', 'ODE Protein c', 'ODE Protein d')
