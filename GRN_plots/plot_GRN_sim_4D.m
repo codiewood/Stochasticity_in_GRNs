e44ead5617cb9dd44c4340ab3d074a7fcf4bd94c
codiewood <52531428+codiewood@users.noreply.github.com>
2020-07-16 20:41:17 +0100

    added promoter state indicator to plots

--- GRN_plots/plot_GRN_sim_4D.m
+++ GRN_plots/plot_GRN_sim_4D.m
@@ -10,23 +10,36 @@
 %       ha; Rate parameter of activator binding
 %       fa; Rate parameter of activator unbinding
 
-inputs = {1,1,1,1};
-[ssa_t, ssa_simdata, ssa_names] = SSA_4D_GRN(inputs);
-[ode_t, ode_simdata, ode_names] = ODE_4D_GRN(inputs);
+inputs = {1,1,1,1,0,0,0,0,5,14,0.1,1e-4,1e-2,2,1e-1};
+Mobj = model_4D_GRN(inputs);
+[ssa_t, ssa_simdata, ssa_names] = SSA_4D_GRN(Mobj);
+[ode_t, ode_simdata, ode_names] = ODE_4D_GRN(Mobj);
 
 %plots figures
 figure;
     colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.929411764705882 0.694117647058824 0.125490196078431;0.494117647058824 0.184313725490196 0.556862745098039;0.301960784313725 0.745098039215686 0.933333333333333;1 0.411764705882353 0.16078431372549;1 1 0.0666666666666667;0 0 0]);
+    title('Molecule numbers vs Time')
     plot(ssa_t, ssa_simdata(:,[2,7,12,17]), 'LineWidth', 1.5)
     hold on
     plot(ode_t, ode_simdata(:,[2,7,12,17]), ':', 'LineWidth', 3)
     xlabel('Time (secs)')
     xlim([0 1000])
     ylabel('Molecule number')
-    title('Molecule numbers vs Time')
+    if [inputs{1:4}] == [1,1,1,1]
+        yyaxis right
+        colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.929411764705882 0.694117647058824 0.125490196078431;0.494117647058824 0.184313725490196 0.556862745098039]);
+        plot(ssa_t, ssa_simdata(:,[4]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
+        plot(ssa_t, 0.99*ssa_simdata(:,[9]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
+        plot(ssa_t, 0.98*ssa_simdata(:,[14]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
+        plot(ssa_t, 0.97*ssa_simdata(:,[19]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
+        ylim([0.01 1.01])
+        set(gca,'YTickLabel',[])
+    end
     legend({'SSA Protein a','SSA Protein b','SSA Protein c','SSA Protein d', ...
-        'ODE Protein a', 'ODE Protein b', 'ODE Protein c', 'ODE Protein d'}, ...
+        'ODE Protein a', 'ODE Protein b', 'ODE Protein c', 'ODE Protein d', ...
+        'Promoter A Activated', 'Promoter B Activated', 'Promoter C Activated', 'Promoter D Activated'}, ...
         'Location', 'eastoutside')
+    
 figure;
     t = tiledlayout(2,2);
     nexttile;
