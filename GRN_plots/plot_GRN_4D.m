19c114c05ad54c694eb6ed0c6c749921c7e787fa
codiewood <52531428+codiewood@users.noreply.github.com>
2020-07-19 16:04:05 +0100

    renamed 4D figure plotter

--- GRN_plots/plot_GRN_4D.m
+++ GRN_plots/plot_GRN_4D.m
@@ -1,4 +1,3 @@
-%simulate SSA and ODE model
 %   inputs = {molA,molB,molC,molD,mola,molb,molc,mold,g0,g1,k,hr,fr,ha,fa}
 %       molA, molB, molC, molD; Number of molecules of unbound promoters A - D
 %       mola, molb, molc, mold; Number of molecules of proteins a - d
@@ -9,12 +8,12 @@
 %       fr; Rate parameter of repressor unbinding
 %       ha; Rate parameter of activator binding
 %       fa; Rate parameter of activator unbinding
-
-inputs = {1,1,1,1,0,0,0,0,5,14,0.1,1e-4,1e-2,2,1e-1};
+%% 
+inputs = {1,1,1,1,0,0,0,0,5,14,1,1e-4,1e-2,2,1e-1};
 Mobj = model_4D_GRN(inputs);
-[ssa_t, ssa_simdata, ssa_names] = SSA_4D_GRN(Mobj);
-[ode_t, ode_simdata, ode_names] = ODE_4D_GRN(Mobj);
-
+[ssa_t, ssa_simdata, ssa_names] = SSA_GRN_sim(Mobj);
+[ode_t, ode_simdata, ode_names] = ODE_GRN_sim(Mobj);
+%% 
 %plots figures
 figure;
     colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.929411764705882 0.694117647058824 0.125490196078431;0.494117647058824 0.184313725490196 0.556862745098039;0.301960784313725 0.745098039215686 0.933333333333333;1 0.411764705882353 0.16078431372549;1 1 0.0666666666666667;0 0 0]);
