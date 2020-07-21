5e6e8b7e27f0e9911b5267d3819291de166f569a
codiewood <52531428+codiewood@users.noreply.github.com>
2020-07-21 16:48:27 +0100

    replaced promoter state plotter to work with multiple promoters

--- GRN_plots/plot_GRN_2D.m
+++ GRN_plots/plot_GRN_2D.m
@@ -8,15 +8,21 @@
 %       fr; Rate parameter of repressor unbinding
 %       ha; Rate parameter of activator binding
 %       fa; Rate parameter of activator unbinding
-%%
+%% 
+% runs simulations and sets default parameter values
 inputs = {1,1,0,0,5,14,1,1e-4,1e-2,2,1e-1};
+numargs = length(inputs);
+args = {1,1,0,0,5,14,1,1e-4,1e-2,2,1e-1};
+args(1:numargs) = inputs;
+[molA,molB,mola,molb,g0,g1,k,hr,fr,ha,fa] = args{:};
+
 Mobj = model_2D_GRN(inputs);
 [ssa_t, ssa_simdata, ssa_names] = SSA_GRN_sim(Mobj);
 [ode_t, ode_simdata, ode_names] = ODE_GRN_sim(Mobj);
 %% 
 %plots figures
 figure;
-    colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.301960784313725 0.745098039215686 0.933333333333333;1 0.411764705882353 0.16078431372549]);
+    colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.301960784313725 0.745098039215686 0.933333333333333;1 0.411764705882353 0.16078431372549;0.301960784313725 0.745098039215686 0.933333333333333;1 0.411764705882353 0.16078431372549]);
     title('Molecule numbers vs Time')
     plot(ssa_t, ssa_simdata(:,[2,7]), 'LineWidth', 1.5)
     hold on
@@ -24,16 +30,13 @@ figure;
     xlabel('Time (secs)')
     xlim([0 1000])
     ylabel('Molecule number')
-    if [inputs{1:2}] == [1,1]
-        yyaxis right
-        colororder([0 0.447058823529412 0.741176470588235;0.850980392156863 0.325490196078431 0.0980392156862745;0.929411764705882 0.694117647058824 0.125490196078431;0.494117647058824 0.184313725490196 0.556862745098039]);
-        plot(ssa_t, ssa_simdata(:,[4]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
-        plot(ssa_t, 0.99*ssa_simdata(:,[9]), 'LineStyle', 'None', 'MarkerSize',10,'Marker','.')
-        ylim([0.01 1.01])
-        set(gca,'YTickLabel',[])
+    for i = [1:2]
+        g_tot = g1*ssa_simdata(:,[5*i-1]) + g0*(ssa_simdata(:,[5*i-4])+ssa_simdata(:,[5*i-2])+ssa_simdata(:,[5*i]));
+        tf = g_tot/k;
+        plot(ssa_t,tf, 'LineStyle', 'None', 'MarkerSize',15,'Marker','.')
     end
     legend({'SSA Protein a','SSA Protein b','ODE Protein a', 'ODE Protein b', ...
-        'Promoter A Activated', 'Promoter B Activated'},'Location', 'eastoutside')
+        'Expected protein a', 'Expected protein b'},'Location', 'eastoutside')
     
 figure;
     t = tiledlayout(1,2);
